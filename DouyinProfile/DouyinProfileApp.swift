d804b9a5490e0ee19c833c31959bb1d32698cd39
Shen <863461783@qq.com>
2024-02-16 22:58:08 +0800

    Initial Commit

---
+++ DouyinProfile/DouyinProfileApp.swift
@@ -0,0 +1,17 @@
+//
+//  DouyinProfileApp.swift
+//  DouyinProfile
+//
+//  Created by 香饽饽zizizi on 2024/2/16.
+//
+
+import SwiftUI
+
+@main
+struct DouyinProfileApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
